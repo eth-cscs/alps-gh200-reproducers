482ab3ad129341b81411b5549ddd123788856f89
Jonathan Coles <jonathan.coles@cscs.ch>
2025-03-14 14:53:36 +0100

    Create new reproducer tcp-bandwidth-variability This reproducer uses iperf3 to perform simple bandwidth checks over slingshot and shows high variability between runs.

---
+++ tcp-bandwidth-variability/README.md
@@ -0,0 +1,4 @@
+```bash
+salloc --reservation=interactive_jobs -w nid[007384,007386] -t 30:00 -N2 -A csstaff --ntasks-per-node=1
+```
+
---
+++ tcp-bandwidth-variability/tcp-bw-var.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#SBATCH -J tcp-bw-var
+#SBATCH -t 1:00:00
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=288
+#SBATCH --exclusive --mem=450G
+
+IPERF="numactl --cpunodebind=0 --membind=0 iperf3"
+
+OFILE=nj
+
+function submit_pair()
+{
+	server=$1
+	client=$2
+
+	SRUN="srun -u -N1 -n1 --exclusive --cpus-per-task=288 --mem=50G"
+	TS="%Y-%m-%dT%H:%M:%S%t"
+	LOG=${OFILE}-${server}-${client}.out
+
+	XNAME_SERVER=$(${SRUN} -w ${server} cat /etc/cray/xname)
+	XNAME_CLIENT=$(${SRUN} -w ${client} cat /etc/cray/xname)
+
+	COMMON_OPTS="--forceflush --timestamps=${TS} --affinity=10 --bind-dev=hsn0"
+
+	SERVER_OPTS="${COMMON_OPTS} -s"
+	${SRUN} -w ${server} ${IPERF} ${SERVER_OPTS}  &
+	SERVER_PID=$!
+
+
+	# wait for server to start
+	sleep 5
+
+
+	echo "server: ${server} ${XNAME_SERVER}" >  ${LOG}
+	echo "client: ${client} ${XNAME_CLIENT}" >> ${LOG}
+
+	for i in $(seq 20); do
+		CLIENT_OPTS="${COMMON_OPTS} -c ${server} --time=5" # --zerocopy --affinity=10 --bitrate=64G"
+		${SRUN} --open-mode=append -o ${LOG} -w ${client} ${IPERF} ${CLIENT_OPTS}
+		sleep 1
+	done
+
+	kill ${SERVER_PID}
+}
+
+
+NODES=$(scontrol show hostnames $SLURM_JOB_NODELIST)
+
+submit_pair ${NODES}
+
+wait
+
