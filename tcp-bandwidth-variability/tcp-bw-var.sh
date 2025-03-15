8fa0caa08bb47caa85b30e06af8999a4ab3e3ea3
Jonathan Coles <jonathan.coles@cscs.ch>
2025-03-15 07:02:53 +0100

    tcp-bandwidth-variability: Update to tcp-bw-var.sh so we don't hard code number of cores in call to srun.

--- tcp-bandwidth-variability/tcp-bw-var.sh
+++ tcp-bandwidth-variability/tcp-bw-var.sh
@@ -14,7 +14,7 @@ function submit_pair()
 	server=$1
 	client=$2
 
-	SRUN="srun -u -N1 -n1 --exclusive --cpus-per-task=24 --mem=50G"
+	SRUN="srun -u -N1 -n1 --exclusive --cpus-per-task=${SLURM_NTASKS_PER_NODE} --mem=50G"
 	TS="%Y-%m-%dT%H:%M:%S%t"
 	LOG=${OFILE}-${server}-${client}.out
 
@@ -32,11 +32,12 @@ function submit_pair()
 	sleep 5
 
 
-	echo "server: ${server} ${XNAME_SERVER}" >  ${LOG}
+	echo "date:   $(date +${TS})"            >  ${LOG}
+	echo "server: ${server} ${XNAME_SERVER}" >> ${LOG}
 	echo "client: ${client} ${XNAME_CLIENT}" >> ${LOG}
 
 	for i in $(seq 20); do
-		CLIENT_OPTS="${COMMON_OPTS} -c ${server} --time=5" # --zerocopy --affinity=10 --bitrate=64G"
+		CLIENT_OPTS="${COMMON_OPTS} -c ${server} --time=20" # --zerocopy --affinity=10 --bitrate=64G"
 		${SRUN} --open-mode=append -o ${LOG} -w ${client} ${IPERF} ${CLIENT_OPTS}
 		sleep 1
 	done
