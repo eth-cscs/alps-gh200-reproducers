d85fd4ba3459ef3d3ddb9b804592f57826345b4c
Jonathan Coles <jonathan.coles@cscs.ch>
2025-10-21 18:57:13 +0200

    Add a new test for the lustre client bug that aggressively scans across OSTs. If a dd call returns "Invalid argument" the bug still exists.

---
+++ stuck-node-on-io/dd_repro-scan-osts.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+OUT=/capstor/store/cscs/cscs/csstaff/jpcoles3
+
+echo "Writing to $OUT"
+echo "HOSTNAME $(hostname)"
+lfs quota -p $(lsattr -p -d $OUT | awk '{print $1}') $OUT -h
+
+sync $OUT/dd_largefile.*
+rm $OUT/dd_largefile.*
+sync
+
+bs=1075761
+count=400
+
+for ((tries=0; tries<=40; tries+=1)); do
+    for ((ost=0; ost<=40; ost+=1)); do
+        lfs setstripe -o $ost ${OUT}
+
+        CMD="dd if=/dev/zero of=$OUT/dd_largefile.${tries}.${ost} bs=${bs} count=${count} status=progress oflag=dsync" 
+        CMD="dd if=/dev/zero of=$OUT/dd_largefile.${tries}.${ost} bs=${bs} count=${count} status=progress" 
+        echo ${CMD}
+        ${CMD}
+
+        if [[ $? != 0 ]]; then
+            lfs quota -p $(lsattr -p -d $OUT | awk '{print $1}') $OUT -h
+            lfs getstripe ${OUT}/dd_largefile.*
+            lfs quota -p $(lsattr -p -d $OUT | awk '{print $1}') $OUT -hv
+        fi
+
+        rm ${OUT}/dd_largefile.*
+        sync
+
+    done
+done
+
